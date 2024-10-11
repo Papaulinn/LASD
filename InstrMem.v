9e62cfa6a358ec8113d4a9b908efa3fc4e9c3523
Papaulinn <paulovictorlima18@gmail.com>
2024-10-11 20:58:54 -0300

    InstrMem.v

--- InstrMem.v
+++ InstrMem.v
@@ -16,18 +16,7 @@ always @(*)
 		8'h14 : RD = 32'hfe409ee3;
 		
 	
-	/*
-		8'h00 : RD = 32'h0ff00083; //lb x1, 0xFF(x0) #Carrega a entrada paralela no registrador 1
-		8'h04 : RD = 32'h0e100fa3; //sb x1, 0xFF(x0) #Salva o registrador 1 na saída paralela
-		8'h08 : RD = 32'hfe000ce3; //beq x0, x0, init #Reinicia o laço
-	
-		8'h00 : RD = 32'h00100193; //lb x1, 0xFF(x0) #Carrega a entrada paralela no registrador 1
-		8'h04 : RD = 32'h0ff00083; 
-		8'h08 : RD = 32'h00108213; 
-		8'h0C : RD = 32'h00327133;
-		8'h10 : RD = 32'h0e200fa3; //sb x1, 0xFF(x0) #Salva o registrador 1 na saída paralela
-		8'h14 : RD = 32'hfe0006e3; //beq x0, x0, init #Reinicia o laço
-		*/
+
 		default: RD = 32'h0;  // instrução default
 	endcase
 
@@ -35,13 +24,3 @@ always @(*)
 endmodule
 
 
-/*
-addi x4, x0, 7
-addi x1, x0,2
-ori x2, x1, 1
-andi x3, x4, 1
-init:
-addi x1, x1, 1 
-testeBne:
-bne x1, x4, init #se a condição de parada for atingida, reinicia a rotina
-*/
